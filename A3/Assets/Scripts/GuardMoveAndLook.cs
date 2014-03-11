2c14d72c654a4f43f36a8bc6e4e0842000548e43
Matthew Adler <madler72@gmail.com>
2014-03-11 16:50:09 -0400

    Main room done (minus some prettifying) Implementing guards now

---
+++ A3/Assets/Scripts/GuardMoveAndLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GuardMoveAndLook : MonoBehaviour {
+
+	public List<GameObject> theWaypoints;
+
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(theWaypoints[0].transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
---
+++ A3/Assets/Scripts/SpawnBlocksAndTreasure.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBlocksAndTreasure : MonoBehaviour {
+
+	public GameObject obstacle1, obstacle2;
+
+	// Use this for initialization
+	void Start () {
+		obstacle1.transform.position = obstacle1.transform.position + new Vector3 (Random.Range(-2.2f, 2.2f), 0, Random.Range(-2.2f, 2.2f));
+		obstacle2.transform.position = obstacle2.transform.position + new Vector3 (Random.Range(-2.2f, 2.2f), 0, Random.Range(-2.2f, 2.2f));
+	
+	
+		obstacle1.transform.Rotate (0, Random.Range(-45, 45), 0); 
+		obstacle2.transform.Rotate (0, Random.Range(-45, 45), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

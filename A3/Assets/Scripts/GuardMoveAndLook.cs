dc624fb8041da4c5cd5d05ce196d5454bdf29675
Matthew Adler <madler72@gmail.com>
2014-03-12 19:01:05 -0400

    Guards moving along waypoints! need to make 8 subrooms instead... :/ so working on going all the way with that

--- A3/Assets/Scripts/GuardMoveAndLook.cs
+++ A3/Assets/Scripts/GuardMoveAndLook.cs
@@ -10,7 +10,7 @@ public class GuardMoveAndLook : MonoBehaviour {
 
 	private GameObject[]allWayPoints;
 
-	private bool atNextPoint, moving;
+	private bool atNextPoint, checkedRoom;
 	private Vector3 startPosition, nextPosition;
 	private GameObject currentWaypoint, nextWaypoint;
 	private int nextPointInList;
@@ -27,16 +27,15 @@ public class GuardMoveAndLook : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		checkedRoom = false;
 		speed = 4.0f;
 		atNextPoint = true;
-		moving = false;
 		transform.LookAt(theWaypoints[0].transform);
 
 		allWayPoints = GameObject.FindGameObjectsWithTag("wayPointTag");
 
 		rand = Random.Range(0, allWayPoints.Length - 1);
 
-		nextPointInList = rand % theWaypoints.Count;
 		transform.position = allWayPoints[rand].transform.position;
 		currentWaypoint = allWayPoints[rand];
 
@@ -54,20 +53,23 @@ public class GuardMoveAndLook : MonoBehaviour {
 
 			//Choose the next waypoint to go to
 			
-			Debug.Log(currentWaypoint.name);
+			Debug.Log("The current waypoints name: " + currentWaypoint.name);
 			
 			if (Regex.IsMatch(currentWaypoint.name, "^*INSIDE"))
 			{
 				//Can only go to currentWaypoints name - INSIDE
 				Debug.Log("Started inside of a room");
 				//Debug.Log(currentWaypoint.name.Substring(0, currentWaypoint.name.Length - 6));
-				nextWaypoint = GameObject.Find(name.Substring(0, currentWaypoint.name.Length - 6));
+				Debug.Log(currentWaypoint.name.Substring(0, currentWaypoint.name.Length - 6));
+				nextWaypoint = GameObject.Find(currentWaypoint.name.Substring(0, currentWaypoint.name.Length - 6));
+				checkedRoom = true;
 
 			}
 			else if (currentWaypoint.name == "MiddleWaypoint")
 			{
 				//can go to any WaypointRoom#
 				Debug.Log("IN DAH MIDDLE!!!!!");
+				checkedRoom = false;
 				nextWaypoint = GameObject.Find("WaypointRoom" + Random.Range(1,6));
 			}
 			else if (Regex.IsMatch(currentWaypoint.name, "Way^*"))
@@ -76,29 +78,73 @@ public class GuardMoveAndLook : MonoBehaviour {
 				Debug.Log("STARED OUTSIDE OF A ROOM");
 
 				//if didn't already look inside... look inside, otherwise go to the next in the list.
+				if (!checkedRoom)
+				{
+					nextWaypoint = GameObject.Find(currentWaypoint.name + "INSIDE");
+				}
+				else
+				{
+					rand = Random.Range(0, 7);
+
+					if (rand == 0)
+					{
+						nextWaypoint = GameObject.Find("MiddleWaypoint");
+						Debug.Log("HEADING THE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MIDDDAIL");
+					}
+					else
+					{
+						for (int i = 0; i < theWaypoints.Count; i++)
+						{
+							if (theWaypoints[i].name.Equals(currentWaypoint.name))
+							{
+								//int j = currentWaypoint.name.Length - 1;
+
+								nextWaypoint = theWaypoints[(i+1) % theWaypoints.Count];//GameObject.Find(currentWaypoint.name.Substring(0, j) + (1 + int.Parse(currentWaypoint.name.Substring(j, 1)) % 6));//
+								 
+								//so that we don't always go to the middle on the last point.
+								if ((i + 1) % theWaypoints.Count == 0)
+									nextWaypoint = theWaypoints[1];
+
+								Debug.Log("FOUND DAH WAYPOINT");
+							}
+						}
+					}
+					//otherwise maybe choose the center from here
+				}
+
+				checkedRoom = false;
 			}
 			else
 			{
 				//can go to the next in the list.
 				Debug.Log("SOME CORNER");
+
+				for (int i = 0; i < theWaypoints.Count; i++)
+				{
+					if (theWaypoints[i].name.Equals(currentWaypoint.name))
+					{
+						nextWaypoint = theWaypoints[(i+1) % theWaypoints.Count];//GameObject.Find(currentWaypoint.name.Substring(0, j) + (1 + int.Parse(currentWaypoint.name.Substring(j, 1)) % 6));//
+						
+						//so that we don't always go to the middle on the last point.
+						if ((i + 1) % theWaypoints.Count == 0)
+							nextWaypoint = theWaypoints[1];
+
+						Debug.Log("FOUND DAH WAYPOINT");
+					}
+				}
 			}
 
-			Debug.Log(nextWaypoint.name);
+			Debug.Log("THE NEXT WAYPOINT IS: " + nextWaypoint.name);
 
-			transform.LookAt(theWaypoints[nextPointInList].transform);
+			transform.LookAt(nextWaypoint.transform);
 
 		
-			nextPosition = theWaypoints[nextPointInList].transform.position;
-
+			nextPosition = nextWaypoint.transform.position;
 
 			journeyLength = Vector3.Distance(startPosition, nextPosition);
 
 			StartCoroutine(toNextWaypoint());
-
-
-			nextPointInList = (nextPointInList + 1) % theWaypoints.Count;
-
-			moving = true;
+			currentWaypoint = nextWaypoint;
 		}
 	
 	}
